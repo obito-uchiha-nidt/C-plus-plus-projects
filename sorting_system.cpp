073084c3093d6ea840938197b3e82d7e61b000ce
obito-uchiha-nidt <tobi3258407@gmail.com>
2026-04-21 14:29:38 +0530

    Add files via upload

---
+++ sorting_system.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {50, 10, 40, 20, 30};
+
+    for(int i = 0; i < 5; i++) {
+        for(int j = i + 1; j < 5; j++) {
+            if(arr[i] > arr[j]) {
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
